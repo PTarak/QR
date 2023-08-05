2b448ffd19260281532c5ed52963ed00e46c31ca
Tarak Potturi <potturitarakanadhareddy@gmail.com>
2023-08-05 22:04:27 +0530

    Initial Commit: Created initial set up of the project

---
+++ src/main/java/com/qr/qr/dto/QrData.java
@@ -0,0 +1,16 @@
+package com.qr.qr.dto;
+
+public class QrData {
+    private String data;
+
+    public String getData() {
+        return data;
+    }
+
+    @Override
+    public String toString() {
+        return "QrData{" +
+                "data='" + data + '\'' +
+                '}';
+    }
+}
